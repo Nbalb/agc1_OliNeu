67b8b9e3362fe2def5412868cddf9f337a6e2d20
Nicola <nbals@protonmail.com>
2023-03-13 18:02:05 +0100

    Fixed alternative splicing analysis

--- code/005_Majiq_diff_splicing.sh
+++ code/005_Majiq_diff_splicing.sh
@@ -1,28 +1,78 @@
-#mamba create -n majiq_env -c http://majiq.biociphers.org/download/channel/linux-64 majiq
-cd mnt/d/onedrive/linux/agc1
+# install majiq folowing their guide: 
+# https://biociphers.bitbucket.io/majiq-docs-academic/getting-started-guide/installing.html
+data=/mnt/d/OneDrive/PhD/projects/agc1_OliNeu/data
+raw_reads=$data/delivery_20200428/raw_reads
+cd $data
+mkdir data/isoforms
 
-### Following (https://biociphers.bitbucket.io/majiq/quick.html#conf_file)
-### Check if reads are stranded/rev_stranded/unstranded
-### Use paramter -s 0(unstranded), 1(stranded), 2(rev stranded) and check the summary
-featureCounts -T 6 -p -s 0 -a gencode.vM25.annotation.gff3 -o agc1feature_unstrand.counts /mnt/d/onedrive/linux/agc1/fastq/wt1.sorted.bam
-featureCounts -T 6 -p -s 1 -a gencode.vM25.annotation.gff3 -o agc1feature_stranded.counts /mnt/d/onedrive/linux/agc1/fastq/wt1.sorted.bam
-featureCounts -T 6 -p -s 2 -a gencode.vM25.annotation.gff3 -o agc1feature_rev_strand.counts /mnt/d/onedrive/linux/agc1/fastq/wt1.sorted.bam
+# Align reads
+mkdir -p isoforms/idx/
+curl -L ftp://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M25/GRCm38.primary_assembly.genome.fa.gz \
+-o isoforms/genome.fa.gz
+gzip -d isoforms/genome.fa.gz
+hisat2-build -p 6 isoforms/genome.fa isoforms/idx/mm10
 
-### Reads are stranded
+trimmed_fastq=$data/isoforms/trimmed_fastq
+bams=$data/isoforms/bams
+mkdir $trimmed_fastq
+mkdir $bams
+
+for i in {1..6}
+do
+
+r1=${raw_reads}/1_ID1603_$i-Olineu${i}_S1_L001_R1_001.fastq.gz
+r2=${raw_reads}/1_ID1603_$i-Olineu${i}_S1_L001_R2_001.fastq.gz
+out_trim1=$trimmed_fastq/Olineu_${i}_R1_trimmed.fastq.gz
+out_trim2=$trimmed_fastq/Olineu_${i}_R2_trimmed.fastq.gz
+report=$trimmed_fastq/fastp_sample${i}.html
+echo "Trimming sample $i"
+fastp -i $r1 -I $r2 -o $out_trim1 -O $out_trim2 -h $report -w 6
+
+out_bam=$bams/Olineu_${i}.bam
+sorted_bam=$bams/Olineu${i}.sorted.bam
+
+echo "Aligning sample $i"
+hisat2 -x isoforms/idx/mm10 -p 6 -1 $out_trim1 -2 $out_trim2 | samtools view -bS - > $out_bam
+
+echo "Sorting sample $i"
+samtools sort -m 3G -@ 6 -O BAM -o $sorted_bam $out_bam && rm $out_bam
+
+echo "Indexing sample $i"
+samtools index $sorted_bam
+done
+
+
+### Reads are forward-stranded
 ### Check settings.ini inside isoforms folder to verify settings and run builder
-cd
-source env/bin/activate
-cd $linux/agc1/isoforms/
-majiq build ../gencode.vM25.annotation.gff3 -c settings.ini -j 4 -o .
+### Create Python environment and install htslib
+curl -OL https://github.com/samtools/htslib/releases/download/1.13/htslib-1.13.tar.bz2
+tar -xf htslib-1.13.tar.bz2  # extract archive
+cd htslib-1.13  # change into htslib source directory
+# configure, make, and install htslib to ~/install/htslib-1.13
+./configure --prefix=$HOME/install/htslib-1.13
+make
+make install
+
+cd $data/isoforms
+curl -OL https://ftp.ebi.ac.uk/pub/databases/gencode/Gencode_mouse/release_M25/gencode.vM25.annotation.gff3.gz
+mkdir $data/isoforms/build
+python3 -m venv majiq_env
+source majiq_env/bin/activate
+
+majiq build gencode.vM25.annotation.gff3.gz -c settings.ini -j 6 -o $data/isoforms/build
 
 ### PSI analysis
-majiq psi wt1.sorted.majiq wt2.sorted.majiq wt3.sorted.majiq -j 4 -o psi/ -n wt
-majiq psi kd1.sorted.majiq kd2.sorted.majiq kd3.sorted.majiq -j 4 -o psi/ -n kd
+cd $data/isoforms/build
+majiq psi Olineu1.sorted.majiq Olineu2.sorted.majiq Olineu3.sorted.majiq -j 6 -o $data/isoforms/psi/ -n wt
+majiq psi Olineu4.sorted.majiq Olineu5.sorted.majiq Olineu6.sorted.majiq -j 6 -o $data/isoforms/psi/ -n kd
 
 ### DeltaPSI analysis
-majiq deltapsi -grp1 wt1.sorted.majiq wt2.sorted.majiq wt3.sorted.majiq -grp2 kd1.sorted.majiq kd2.sorted.majiq kd3.sorted.majiq -j 4 -o psi/ -n wt kd
+majiq deltapsi \
+-grp1 Olineu1.sorted.majiq Olineu2.sorted.majiq Olineu3.sorted.majiq \
+-grp2 Olineu4.sorted.majiq Olineu5.sorted.majiq Olineu6.sorted.majiq \
+-j 6 -o $data/isoforms/psi/ -n wt kd
 
 ### Voila visualization
-voila tsv splicegraph.sql psi/wt_kd.deltapsi.voila -f voila_results
-voila view -p 59408 splicegraph.sql psi/wt_kd.deltapsi.voila   
+voila tsv splicegraph.sql $data/isoforms/psi/wt-kd.deltapsi.voila -f voila_results
+voila view -p 59408 splicegraph.sql $data/isoforms/psi/wt-kd.deltapsi.voila   
 
